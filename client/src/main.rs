d2fd07e2379ffb7d917147db55762130f5c9a3a7
Tom <tom.meyer89@gmail.com>
2018-07-14 20:36:37 +0200

    simple udp broadcast for broker lookup

---
+++ client/src/main.rs
@@ -0,0 +1,206 @@
+extern crate bytes;
+extern crate futures;
+extern crate leif_common;
+extern crate tokio;
+extern crate tokio_codec;
+extern crate tokio_io;
+
+use futures::sync::mpsc;
+use leif_common::get_broadcasts;
+use std::io::{self, Error, ErrorKind, Read, Result, Write};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::time::{Duration, Instant};
+use tokio::net::UdpSocket;
+use tokio::prelude::*;
+use tokio::runtime::Runtime;
+
+fn get_broker() -> Result<SocketAddr> {
+    const MAX_DATAGRAM_SIZE: usize = 65_507;
+    for broadcast in get_broadcasts().unwrap() {
+        // 8207 => BROT(cast)
+        // setup socket
+        let remote_addr = SocketAddr::new(broadcast, 8207);
+        println!("Broadcast to {}", remote_addr);
+        let socket = UdpSocket::bind(&"[::]:0".parse().unwrap()).unwrap();
+        socket.set_broadcast(true).unwrap();
+        //define task
+        let request_broker_ip = socket
+            .send_dgram("SearchingBroker", &remote_addr)
+            .and_then(|(socket, _)| socket.recv_dgram(vec![0u8; MAX_DATAGRAM_SIZE]))
+            .deadline(Instant::now() + Duration::from_secs(2))
+            .map_err(|e| Error::new(ErrorKind::TimedOut, "No answer in this net"));
+        // execute task
+        let mut runtime = Runtime::new().unwrap();
+        let result = runtime.block_on(request_broker_ip);
+        let (socket, data, len, remote_addr) = result.unwrap();
+        return Ok(remote_addr);
+    }
+    Err(Error::new(ErrorKind::NotFound, "Unable to find broker"))
+}
+
+fn main() {
+    let broker = get_broker();
+    println!("found broker: {}", broker.unwrap());
+
+    // let mut args = env::args().skip(1).collect::<Vec<_>>();
+    // let tcp = match args.iter().position(|a| a == "--udp") {
+    //     Some(i) => {
+    //         args.remove(i);
+    //         false
+    //     }
+    //     None => true,
+    // };
+
+    // let addr = args.first()
+    //     .unwrap_or_else(|| panic!("this program requires at least one argument"));
+    // let addr = addr.parse::<SocketAddr>().unwrap();
+
+    // let (stdin_tx, stdin_rx) = mpsc::channel(0);
+    // thread::spawn(|| read_stdin(stdin_tx));
+    // let stdin_rx = stdin_rx.map_err(|_| panic!());
+    // let stdout = if tcp {
+    //     tcp::connect(&addr, Box::new(stdin_rx))
+    // } else {
+    //     udp::connect(&addr, Box::new(stdin_rx))
+    // };
+
+    // let mut out = io::stdout();
+
+    // tokio::run({
+    //     stdout
+    //         .for_each(move |chunk| out.write_all(&chunk))
+    //         .map_err(|e| println!("error reading stdout; error = {:?}", e))
+    // });
+}
+
+mod codec {
+    use bytes::{BufMut, BytesMut};
+    use std::io;
+    use tokio_codec::{Decoder, Encoder};
+
+    pub struct Bytes;
+
+    impl Decoder for Bytes {
+        type Item = BytesMut;
+        type Error = io::Error;
+
+        fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<BytesMut>> {
+            if buf.len() > 0 {
+                let len = buf.len();
+                Ok(Some(buf.split_to(len)))
+            } else {
+                Ok(None)
+            }
+        }
+    }
+
+    impl Encoder for Bytes {
+        type Item = Vec<u8>;
+        type Error = io::Error;
+
+        fn encode(&mut self, data: Vec<u8>, buf: &mut BytesMut) -> io::Result<()> {
+            buf.put(&data[..]);
+            Ok(())
+        }
+    }
+}
+
+mod tcp {
+    use tokio;
+    use tokio::net::TcpStream;
+    use tokio::prelude::*;
+    use tokio_codec::Decoder;
+
+    use bytes::BytesMut;
+    use codec::Bytes;
+
+    use std::io;
+    use std::net::SocketAddr;
+
+    pub fn connect(
+        addr: &SocketAddr,
+        stdin: Box<Stream<Item = Vec<u8>, Error = io::Error> + Send>,
+    ) -> Box<Stream<Item = BytesMut, Error = io::Error> + Send> {
+        let tcp = TcpStream::connect(addr);
+
+        Box::new(tcp.map(move |stream| {
+            let (sink, stream) = Bytes.framed(stream).split();
+
+            tokio::spawn(stdin.forward(sink).then(|result| {
+                if let Err(e) = result {
+                    panic!("failed to write to socket: {}", e)
+                }
+                Ok(())
+            }));
+
+            stream
+        }).flatten_stream())
+    }
+}
+
+mod udp {
+    use std::io;
+    use std::net::SocketAddr;
+
+    use bytes::BytesMut;
+    use tokio;
+    use tokio::net::{UdpFramed, UdpSocket};
+    use tokio::prelude::*;
+
+    use codec::Bytes;
+
+    pub fn connect(
+        &addr: &SocketAddr,
+        stdin: Box<Stream<Item = Vec<u8>, Error = io::Error> + Send>,
+    ) -> Box<Stream<Item = BytesMut, Error = io::Error> + Send> {
+        let addr_to_bind = if addr.ip().is_ipv4() {
+            "0.0.0.0:0".parse().unwrap()
+        } else {
+            "[::]:0".parse().unwrap()
+        };
+        let udp = UdpSocket::bind(&addr_to_bind).expect("failed to bind socket");
+
+        let (sink, stream) = UdpFramed::new(udp, Bytes).split();
+
+        let forward_stdin = stdin
+            .map(move |chunk| (chunk, addr))
+            .forward(sink)
+            .then(|result| {
+                if let Err(e) = result {
+                    panic!("failed to write to socket: {}", e)
+                }
+                Ok(())
+            });
+
+        let receive = stream.filter_map(move |(chunk, src)| {
+            if src == addr {
+                Some(chunk.into())
+            } else {
+                None
+            }
+        });
+
+        Box::new(
+            future::lazy(|| {
+                tokio::spawn(forward_stdin);
+                future::ok(receive)
+            }).flatten_stream(),
+        )
+    }
+}
+
+fn read_stdin(mut tx: mpsc::Sender<Vec<u8>>) {
+    let mut stdin = io::stdin();
+    loop {
+        let mut buf = vec![0; 1024];
+        let n = match stdin.read(&mut buf) {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+        buf.truncate(n);
+        tx = match tx.send(buf).wait() {
+            Ok(tx) => tx,
+            Err(_) => break,
+        };
+    }
+}
---
+++ common/src/lib.rs
@@ -0,0 +1,29 @@
+extern crate get_if_addrs;
+
+use get_if_addrs::IfAddr;
+use std::io::Result;
+use std::net::IpAddr;
+
+pub fn get_ip_addresses() -> Result<Vec<IpAddr>> {
+    let mut ret = Vec::new();
+    for iface in get_if_addrs::get_if_addrs()? {
+        if !iface.is_loopback() {
+            ret.push(iface.ip());
+        }
+    }
+    Ok(ret)
+}
+
+pub fn get_broadcasts() -> Result<Vec<IpAddr>> {
+    let mut ret: Vec<IpAddr> = Vec::new();
+    for iface in get_if_addrs::get_if_addrs()? {
+        match iface.addr {
+            IfAddr::V4(v4_iface) => match v4_iface.broadcast {
+                Some(broadcast) => ret.push(IpAddr::V4(broadcast)),
+                None => {}
+            },
+            IfAddr::V6(v6_iface) => {}
+        }
+    }
+    Ok(ret)
+}
